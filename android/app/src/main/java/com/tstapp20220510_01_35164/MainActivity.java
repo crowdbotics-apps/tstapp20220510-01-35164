76655576cf0c8ca176df193430072f089c624d4e
Crowdbotics <team@crowdbotics.com>
2022-05-10 22:01:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tstapp20220510_01_35164/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tstapp20220510_01_35164;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tstapp20220510_01_35164";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tstapp20220510_01_35164",
+  url: "https://tstapp20220510_01_35164.botics.co",
+  api: "https://tstapp20220510_01_35164.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
